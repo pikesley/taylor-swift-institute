ba77879d2d5575df16685ab8018b51de03ee5545
pikesley <github@orgraphone.org>
2015-06-24 08:55:18 +0100

    I got a blank space, baby
    For this commit message

---
+++ README.md
@@ -0,0 +1,1 @@
+# The Taylor Swift Institute
---
+++ content_script.js
@@ -0,0 +1,43 @@
+walk(document.body);
+
+document.title = replaceText(document.title);
+
+function walk(node)
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+	switch ( node.nodeType )
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) {
+  textNode.nodeValue = replaceText(textNode.nodeValue);
+}
+
+function replaceText(v)
+{
+	// Taylor
+	v = v.replace(/\bOpen Data Institute\b/g, "Taylor Swift Institute");
+	v = v.replace(/\bODI\b/g, "TSI");
+
+	return v;
+}
